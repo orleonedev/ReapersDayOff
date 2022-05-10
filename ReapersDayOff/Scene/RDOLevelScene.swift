a25df56b54e1d1cf79d5a85251c04ee543179530
Maria Smirnova <92306135+MarieMittu@users.noreply.github.com>
2022-05-10 15:57:22 +0200

    fixed level scene

--- ReapersDayOff/Scene/RDOLevelScene.swift
+++ ReapersDayOff/Scene/RDOLevelScene.swift
@@ -116,14 +116,14 @@ class RDOLevelScene: RDOBaseScene, SKPhysicsContactDelegate {
     lazy var componentSystems: [GKComponentSystem] = {
         let agentSystem = GKComponentSystem(componentClass: SoulAgent.self)
         let animationSystem = GKComponentSystem(componentClass: AnimationComponent.self)
-//        let chargeSystem = GKComponentSystem(componentClass: ChargeComponent.self)
+        let chargeSystem = GKComponentSystem(componentClass: ChargeComponent.self)
         let intelligenceSystem = GKComponentSystem(componentClass: IntelligenceComponent.self)
-//        let movementSystem = GKComponentSystem(componentClass: MovementComponent.self)
-//        let beamSystem = GKComponentSystem(componentClass: BeamComponent.self)
+        let movementSystem = GKComponentSystem(componentClass: MovementComponent.self)
+
 //        let rulesSystem = GKComponentSystem(componentClass: RulesComponent.self)
 
         // The systems will be updated in order. This order is explicitly defined to match assumptions made within components.
-        return [intelligenceSystem, agentSystem, animationSystem]
+        return [intelligenceSystem, movementSystem, agentSystem, chargeSystem, animationSystem]
     }()
     
     // MARK: Initializers
@@ -572,100 +572,4 @@ class RDOLevelScene: RDOBaseScene, SKPhysicsContactDelegate {
     }
   
 }
-
-
-/*
-import SpriteKit
-import GameplayKit
-
-class RDOStageOneScene: RDOBaseScene {
-    // MARK: Properties
-    
-    /// Returns the background node from the scene.
-    override var backgroundNode: SKSpriteNode? {
-        return childNode(withName: "backgroundNode") as? SKSpriteNode
-    }
-    
-    /// The "NEW GAME" button which allows the player to proceed to the first level.
-    var proceedButton: RDOButtonNode? {
-        return backgroundNode?.childNode(withName: ButtonIdentifier.home.rawValue) as? RDOButtonNode
-    }
-    
-    // MARK: Pathfinding
-    var graphs = [String : GKGraph]()
-    
-    lazy var obstacleSpriteNodes: [SKSpriteNode] = self["world/obstacles/*"] as! [SKSpriteNode]
-     
-    lazy var polygonObstacles: [GKPolygonObstacle] = SKNode.obstacles(fromNodePhysicsBodies: self.obstacleSpriteNodes)
-     
-    lazy var graph: GKObstacleGraph = GKObstacleGraph(obstacles: self.polygonObstacles, bufferRadius: GameplayConfiguration.Soul.pathfindingGraphBufferRadius)
-    
-
-    /// An array of objects for `SceneLoader` notifications.
-    private var sceneLoaderNotificationObservers = [Any]()
-
-    // MARK: Deinitialization
-    
-    deinit {
-        // Deregister for scene loader notifications.
-        for observer in sceneLoaderNotificationObservers {
-            NotificationCenter.default.removeObserver(observer)
-        }
-    }
-    
-    // MARK: Scene Life Cycle
-
-    override func didMove(to view: SKView) {
-        super.didMove(to: view)
-        // Enable focus based navigation.
-        focusChangesEnabled = true
-        
-//        registerForNotifications()
-        centerCameraOnPoint(point: backgroundNode!.position)
-      
-        
-        
-    }
-    /*
-    func registerForNotifications() {
-        // Only register for notifications if we haven't done so already.
-        guard sceneLoaderNotificationObservers.isEmpty else { return }
-        
-        // Create a closure to pass as a notification handler for when loading completes or fails.
-        let handleSceneLoaderNotification: (Notification) -> () = { [unowned self] notification in
-            let sceneLoader = notification.object as! SceneLoader
-            
-            // Show the proceed button if the `sceneLoader` pertains to a `LevelScene`.
-            if sceneLoader.sceneMetadata.sceneType is LevelScene.Type {
-                // Allow the proceed and screen to be tapped or clicked.
-                self.proceedButton?.isUserInteractionEnabled = true
-                self.screenRecorderButton?.isUserInteractionEnabled = true
-
-                // Fade in the proceed and screen recorder buttons.
-                self.screenRecorderButton?.run(SKAction.fadeIn(withDuration: 1.0))
-
-                // Clear the initial `proceedButton` focus.
-                self.proceedButton?.isFocused = false
-                self.proceedButton?.run(SKAction.fadeIn(withDuration: 1.0)) {
-                    // Indicate that the `proceedButton` is focused.
-                    self.resetFocus()
-                }
-            }
-        }
-        
-        /*
-        // Register for scene loader notifications.
-        let completeNotification = NotificationCenter.default.addObserver(forName: NSNotification.Name.SceneLoaderDidCompleteNotification, object: nil, queue: OperationQueue.main, using: handleSceneLoaderNotification)
-        let failNotification = NotificationCenter.default.addObserver(forName: NSNotification.Name.SceneLoaderDidFailNotification, object: nil, queue: OperationQueue.main, using: handleSceneLoaderNotification)
-         */
-        
-        // Keep track of the notifications we are registered to so we can remove them in `deinit`.
-        sceneLoaderNotificationObservers += [completeNotification, failNotification]
-        
-    }
-    */
-}
-*/
-*/
-                                                                    
 
